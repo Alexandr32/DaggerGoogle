369ba402d87e145b277f7d47ffe135136f2e7d4b
Alex <alex3289.aa@gmai.com>
2020-09-12 18:37:12 +0500

    внедрение зависимостей в mainActivity

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.registration.RegistrationViewModel
 import dagger.BindsInstance
@@ -9,9 +10,6 @@ import dagger.Component
 // говорит где использовать внедрение зависимостей
 @Component(modules = [StorageModule::class])
 interface AppComponent {
-    //сообщаем Dagger, что RegistrationActivity запрашивает инъекцию и что он должен предоставить
-    // зависимости, которые аннотируются с помощью @Inject (то есть, RegistrationViewModel).
-    fun inject(registrationActivity: RegistrationActivity)
 
     // фабрика для контекста
     // Factory to create instances of the AppComponent
@@ -20,4 +18,10 @@ interface AppComponent {
         // With @BindsInstance, the Context passed in will be available in the graph
         fun create(@BindsInstance context: Context): AppComponent
     }
+
+    // сообщаем Dagger, что RegistrationActivity запрашивает инъекцию и что он должен предоставить
+    // зависимости, которые аннотируются с помощью @Inject (то есть, RegistrationViewModel).
+    fun inject(registrationActivity: RegistrationActivity)
+
+    fun inject(activity: MainActivity)
 }
